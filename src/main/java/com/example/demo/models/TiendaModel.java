e0fd36a4bd195c1491e54a4cfafd85d5db2bcac2
DiegoCalvo1996 <dacalvog@uqvirtual.edu.co>
2022-04-16 17:39:14 -0500

    Se modifico la clase TiendaModel

--- src/main/java/com/example/demo/models/TiendaModel.java
+++ src/main/java/com/example/demo/models/TiendaModel.java
@@ -1,5 +1,66 @@
 package com.example.demo.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tiendas")
 public class TiendaModel {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+	private long id;
+	
+	@Column(nullable = false)
+	private String nit;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	@Column(nullable = false)
+	private String descripcionReporte;
+	
+	public TiendaModel() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcionReporte() {
+		return descripcionReporte;
+	}
+
+	public void setDescripcionReporte(String descripcionReporte) {
+		this.descripcionReporte = descripcionReporte;
+	}
+	
+	
 }
